32a1732232b18e2933916e7de5d0e238d94aa178
VesnaBrucoms <etstevens60@gmail.com>
2016-09-01 23:06:49 +0100

    Added LICENSE, added MANIFEST, updated setup with classifiers and changed description.

--- setup.py
+++ setup.py
@@ -3,8 +3,18 @@ from distutils.core import setup
 
 setup(name='Ini Handler',
       version='0.1',
-      description='Python library for handling ini files',
+      description='Ini file handler',
       author='Trevalyan Stevens',
       author_email='etstevens60@gmail.com',
       url='http://www.kerneweksoftware.com/software/',
-      packages=['ini_handler'])
+      packages=['ini_handler'],
+      classifiers=[
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+      ])
