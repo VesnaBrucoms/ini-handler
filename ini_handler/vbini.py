e9da5d3d7f944a0d7f3b4931daf2d7e04739a4b4
VesnaBrucoms <etstevens60@gmail.com>
2016-09-02 19:21:28 +0100

    Added filename and directory getters, dummy load-save-iter methods, and arg setup during init.

--- ini_handler/vbini.py
+++ ini_handler/vbini.py
@@ -1,30 +1,45 @@
 """The main object for Ini Handler"""
+import sys
+import os
 
 
 class Ini(object):
 
-    def __init__(self, filename='settings', location=None):
-        self._filename = filename
-        self._location = location
+    def __init__(self, filename='settings', directory=None):
         self._settings = {}
+
+        if filename.endswith('.ini'):
+            self._filename = filename[:-4]
+        else:
+            self._filename = filename
+
+        if directory is None:
+            self._directory = os.path.expanduser('~')
+            if sys.platform == 'win32':
+                self._directory += '\\My Documents'
+        else:
+            self._directory = directory
 
     def __delitem__(self, key):
         if not type(key) == str:
             raise TypeError('Key must be of type string')
 
-        if key in self._settings:
+        try:
             self._settings.pop(key)
-        else:
+        except KeyError:
             raise KeyError('{} not found'.format(key))
 
     def __getitem__(self, key):
         if not type(key) == str:
             raise TypeError('Key must be of type string')
 
-        if key in self._settings:
+        try:
             return self._settings[key]
-        else:
+        except KeyError:
             raise KeyError('{} not found'.format(key))
+
+    def __iter__(self):
+        pass
 
     def __len__(self):
         return len(self._settings)
@@ -36,4 +51,18 @@ class Ini(object):
         self._settings[key] = value
 
     def __str__(self):
-        return str(self._settings)
+        return self._directory + '\\' + self._filename + '.ini'
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def directory(self):
+        return self._directory
+
+    def load(self):
+        pass
+
+    def save(self):
+        pass
