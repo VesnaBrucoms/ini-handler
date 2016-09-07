2ebe33f4ece4dba2bb1e1ea39d37b7700b65d12f
VesnaBrucoms <etstevens60@gmail.com>
2016-09-07 22:31:25 +0100

    Now using Invoke and Sphinx

--- ini_handler/__init__.py
+++ ini_handler/__init__.py
@@ -1,6 +1,7 @@
 from pkgutil import extend_path
 
-__path__ = extend_path(__path__, __name__)
+if __name__ == '__main__':
+    __path__ = extend_path(__path__, __name__)
 
 library_details = {
     'version': '0.3.1',
---
+++ tasks.py
@@ -0,0 +1,26 @@
+from invoke import run
+from invoke import task
+
+
+@task
+def clean(ctx):
+    ctx.run('rd /S /Q .\\build')
+    ctx.run('rd /S /Q .\\dist')
+    ctx.run('del .coverage')
+
+
+@task
+def build_docs(ctx):
+    print('Building documentation...')
+    ctx.run('make clean')
+    ctx.run('make html')
+    ctx.run('sphinx-apidoc -o ./_build ../ini_handler')
+    print('Complete')
+
+
+@task
+def run_unit_tests(ctx):
+    print('Starting tests...')
+    ctx.run('nosetests -v --with-coverage --cover-erase \
+            --cover-package ./tests/unit-tests')
+    print('Complete')
